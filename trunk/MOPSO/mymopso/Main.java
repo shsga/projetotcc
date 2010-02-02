e0434221f6021745e6cbac253d85e87b47d40633
periclesmiranda <periclesmiranda@2e2649c8-0f90-11df-9648-19674e83c9f2>
2010-02-02 00:42:08 +0000

    Projeto inicial passado pelo email.

---
+++ trunk/MOPSO/mymopso/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mymopso;
+
+import util.Util;
+/**
+ *
+ * @author Elliackin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Particle p1 = new Particle(0);
+        Particle p2 = new Particle(1);
+        double[] f1 = {0,0,4,1};
+        double[] f2 = {0,0,0,1};
+     
+    }
+
+}
---
+++ trunk/MOPSO/util/Constants.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package util;
+
+/**
+ *
+ * @author Elliackin
+ */
+public class Constants {
+
+    
+    public static final int DOMINATED = -1;
+    public static final int INCOMPARABLE = 0;
+    public static final int DOMINATES = 1;
+
+    //Constantes do MOPSO
+    public static final int SIZE_OF_SWARM = 100;
+    public static final int TOT_ITERATIONS = 100;
+    public static final double MUTATION_RATE = 0.5;
+    public static final int NUM_OF_GRIDS = 30;
+    public static final int EXTERNAL_ARCHIVE_SIZE = 200;
+    public static final double X = 10.0;
+
+    //Constantes gerais do PSO
+    public static final double C1 = 1.0;
+    public static final double C2 = 1.0;
+    public static final double W = 0.4;
+
+}
+
+
+
